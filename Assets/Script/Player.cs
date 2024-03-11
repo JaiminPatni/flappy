02efa3cd33d684b687d968f1421963c649c1580d
JaiminPatni <jaiminpatni8160@gmail.com>
2024-03-11 11:29:40 +0530

    continue

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,15 +8,20 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
-            direction = Vector3.up;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            direction = Vector3.up * strength;
         }
 
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
         }
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
     }
 }
