aa6c53ccd14263f94e1eddc7dc6c7eb027cfc6f4
JaiminPatni <jaiminpatni8160@gmail.com>
2024-03-12 13:40:19 +0530

    continue

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -19,11 +19,12 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+        InvokeRepeating(nameof(Animatesprite), 0.15f, 0.15f);
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
             direction = Vector3.up * strength;
         }
 
@@ -39,12 +40,24 @@ public class Player : MonoBehaviour
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
     }
-    private void AnimateSprite()
+    private void Animatesprite()
     {
         spriteIndex++;
-        if(spriteIndex >= sprites.Length){
+        if (spriteIndex >= sprites.Length)
+        {
             spriteIndex = 0;
         }
-        spriteIndex.sprite = sprites[spriteIndex];
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            FindObjectsOfType<GameManager>().GameOver();
+        }
+        else if (other.gameObject.tag == "Scoring")
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
     }
 }
